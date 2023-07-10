8143600fc2d20ff54a82a2290ea4b5f06d73b307
Temmy3620 <me512papiko512@gmail.com>
2023-07-11 00:03:15 +0900

    日付とコメントどちらも抽出できるようにした

--- thread_input.py
+++ thread_input.py
@@ -1,36 +1,40 @@
-import sqlite3
+import sys
 
 import requests
 from bs4 import BeautifulSoup
 
-dbname = 'thread.db'
-
-conn=sqlite3.connect(dbname)
-c = conn.cursor()
-"""
-sql = 'insert into threadlab (id, comment) values (?,?)'
-
-# Webページを取得して解析する
-load_url = "https://jbbs.shitaraba.net/bbs/read_archive.cgi/anime/11133/1686317062/"
-html = requests.get(load_url)
-soup = BeautifulSoup(html.content, "html.parser")
 
 
-for element in soup.find_all("dd"):    # その中のliタグの文字列を表示
-    print(element.text)
-    
-"""
 def thread_input(load_url):
-    
-    sql = 'insert into threadlab (id, comment) values (?,?)'
+    """
+    スレのURLを読み込んで全ての日付とコメントを返す
+    """
 
     # Webページを取得して解析する
-    #load_url = "https://jbbs.shitaraba.net/bbs/read_archive.cgi/anime/11133/1686317062/"
-    html = requests.get(load_url)
+    html = requests.get(load_url)#URLを読み込む
     soup = BeautifulSoup(html.content, "html.parser")
-
-
-    for element in soup.find_all("dd"):    # その中のliタグの文字列を表示
-        print(element.text)
     
+    
+    try:
+        for num in range(2998,3002):#コメント読み込む
+            element = soup.find(id="comment_" + str(num))    # その中のliタグの文字列を表示
+            
+            print(element.text)#日付
+                
+            next_tag = element.find_next_sibling()
+            print(next_tag.text)#コメント
+    
+    except AttributeError:#不明な行を読み込んだ際
+        print("None...")
+        sys.exit()  # 処理を終了する
+    
+    #num +=1
+
+def date_get():
+    """
+    タグの要素を取得して３つ目の
+    """
+    
+    return 0
+
 thread_input("https://jbbs.shitaraba.net/bbs/read_archive.cgi/anime/11133/1686317062/")
